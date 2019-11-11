8b16dd8e567c70e05caeb343d4116e3eed199223
saviourdog <1690364680@qq.com>
2019-11-11 14:19:19 +0800

    reduce

--- lib/login/index.dart
+++ lib/login/index.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 
 import 'package:animationtest/wave_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_set/animation_set.dart';
-import 'package:flutter_animation_set/animator.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -11,9 +9,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  var 
-      textConNum = TextEditingController(),
-      focusNodeNum = FocusNode();
+  var focusNodeNum = FocusNode();
 
   @override
   void initState() {
@@ -22,12 +18,6 @@ class _LoginState extends State<Login> {
       FocusScope.of(context).requestFocus(focusNodeNum);
     });
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -40,48 +30,27 @@ class _LoginState extends State<Login> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             WaveText(
-              'Hi,',
-              delay: 200,
-              split: ' ',
-            ),
-            WaveText(
               'can i have your number ?',
               delay: 700,
             ),
-            AnimatorSet(
-              animationType: AnimationType.once,
-              animatorSet: [
-                Delay(duration: 2000),
-                Serial(duration: 700, serialList: [
-                  TY(from: 40, to: 0, curve: Curves.ease),
-                  O(from: 0, to: 1, curve: Curves.ease)
-                ]),
-              ],
-              child: TextField(
-                controller: textConNum,
-                focusNode: focusNodeNum,
-                onChanged: (_) {
-                  setState(() {});
-                },
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    fillColor: Colors.black,
-                    hintText: '请输入手机号码',
-
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
-                      ),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    )),
-                textInputAction: TextInputAction.next,
-              ),
+            TextField(
+              focusNode: focusNodeNum,
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  fillColor: Colors.black,
+                  hintText: '请输入手机号码',
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  )),
+              textInputAction: TextInputAction.next,
             )
           ],
         ),
